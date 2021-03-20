a1d45d9f50f87175c49fb9183037b6a4b0b8e0f1
Daniel Alves <danielribeiroalves0@gmail.com>
2021-03-20 18:10:26 -0300

    Classe abstrata operacoes

---
+++ DespesasV01/src/Operacoes.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.*;
+import org.javatuples.Pair; 
+
+public abstract class Operacoes {
+	protected abstract double ColetarValor();
+	protected abstract LocalDate ColetarData();
+	protected abstract String ColetarDescricao();
+	protected abstract Map<LocalDate, Pair<Double, String>> Armazenar(LocalDate data, double valor, String desc);
+	protected abstract Stack<Double> Armazenar(double valor);
+	
+	
+	
+}
