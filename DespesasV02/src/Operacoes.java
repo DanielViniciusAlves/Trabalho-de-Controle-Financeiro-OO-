a402b34bc6e0fa0dfcbc2d214991d57ca609774f
Daniel Alves <danielribeiroalves0@gmail.com>
2021-03-31 00:37:31 -0300

    Classe abstrata de Despesa e Receita

---
+++ DespesasV02/src/Operacoes.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+//Clase Abstrata Operacoes de Despesa e Receita
+public abstract class Operacoes {
+	
+	protected abstract double ColetarValor();
+	
+	protected abstract LocalDate ColetarData();
+	
+	protected abstract String ColetarDescricao();
+	
+	
+	
+	
+}
