25dfa9bc2645bff51722366a8a626d62e1f976cc
Daniel Alves <danielribeiroalves0@gmail.com>
2021-04-08 23:02:58 -0300

    Classe para obter dados de arquivo do tipo .txt, e armazenar esses dados em um Hashmap

---
+++ DespesasV02/src/LeArquivo.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class LeArquivo {
+	private HashMap<LocalDate, SaldoDiario> mapa;
+	private String filePath;
+	
+	//Construtor
+	public LeArquivo(HashMap<LocalDate, SaldoDiario> mapa, String filePath) {
+		this.mapa = mapa;
+		this.filePath = filePath;
+	}
+	
+	//Deleta dados do arquivo para armazenar novos dados
+	private void DeletaArquivo() throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(this.filePath);
+		writer.print("");
+		writer.close();
+	}
+	
+	public HashMap<LocalDate, SaldoDiario> PreencheMapa(Saldo saldo) throws FileNotFoundException{
+        BufferedReader br = null;
+  
+        try {
+  
+            // cria object de arquivo
+            File file = new File(filePath);
+  
+            // cria BufferedReader object do arquivo
+            br = new BufferedReader(new FileReader(file));
+  
+            String line = null;
+  
+            // le linha por linha
+            while ((line = br.readLine()) != null) {
+  
+                // separa informocoes pelo :
+                String[] parts = line.split(":");
+  
+                // primeira parte sera a data
+                String dataArquivo = parts[0].trim();
+                LocalDate data = LocalDate.parse(dataArquivo);
+                // segunda parte sera a descricao
+                String descricao = parts[1].trim();
+                // terceira parte sera o valor
+                String valorArquivo = parts[2].trim();
+                Double valor = Double.parseDouble(valorArquivo); 
+  
+                // put data, descricao e valor no mapa se nao estiverem vazias
+                if (!dataArquivo.equals("") && !descricao.equals("") && !valorArquivo.equals("")) {
+                	saldo.AdicionaSaldoDiario(valor, descricao, data);
+                }
+             
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+  
+            // Fechando o BufferedReader
+            if (br != null) {
+                try {
+                    br.close();
+                }
+                catch (Exception e) {
+                };
+            }
+        }
+        DeletaArquivo();
+        return mapa;
+    }
+}
