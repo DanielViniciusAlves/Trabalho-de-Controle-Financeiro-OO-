cc0987f8822bdef1225a70bb2ebefdbb2a08c12b
Daniel Alves <danielribeiroalves0@gmail.com>
2021-04-16 00:09:26 -0300

    Adicionando Main ao MVC

---
+++ DespesasV02/src/controle/Main.java
@@ -0,0 +1,10 @@
+package controle;
+import java.io.FileNotFoundException;
+import view.ContaBancaria;
+
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+		//Classe responsasavel pelo Menu
+		ContaBancaria.Menu();
+	}
+}
