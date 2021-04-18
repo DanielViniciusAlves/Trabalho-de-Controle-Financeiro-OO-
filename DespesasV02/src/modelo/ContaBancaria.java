f7699df61b06ba4ed18979ca7126f659a374bf5b
Daniel Alves <danielribeiroalves0@gmail.com>
2021-04-18 15:53:18 -0300

    Deletando menu de terminal

--- DespesasV02/src/modelo/ContaBancaria.java
+++ DespesasV02/src/modelo/ContaBancaria.java
@@ -40,62 +40,6 @@ public class ContaBancaria {
 		LeArquivoControle.PreencheSaldo(saldo);
 		
 		Menu frame = new Menu(mapa, filePathMapa, filePathSaldo, despesaControle, receitaControle, saldo);
-//		//Objeto para ler input
-//		Scanner read = new Scanner(System.in);
-//		char option;
-//		String descricao;
-//		do {
-//			//Imprimindo Menu
-//			System.out.print("Digite a opcao desejada:\n"); 
-//	        System.out.print("1.Adicionar despesa:\n"); 
-//	        System.out.print("2.Adicionar receita:\n");
-//	        System.out.print("3.Historico de operacoes:\n");
-//	        System.out.print("4.Saldo diario:\n");
-//	        System.out.print("5.Saldo total:\n");
-//	        System.out.print("Digite '0' para sair do programa\n");
-//	        option = read.next().charAt(0); 
-//	        //Switch para opcoes
-//	        switch(option) {
-//	        
-//	        	case('1'):
-//	        		//despesaControle.AddDespesa();
-//	        		break;
-//	        	case('2'):
-//	        		//receitaControle.AddReceita();
-//	        		break;
-//	        	case('3'):
-//	        		System.out.print("Digite a Data(yyyy-MM-dd) ");
-//		    		data = LocalDate.parse(read.next(), formatoData);
-//		    		
-//		    		System.out.print("Digite a opcao desejada:\n"); 
-//			        System.out.print("1.Vizualizar operacoes:\n"); 
-//			        System.out.print("2.Remover operacao:\n");
-//			        option = read.next().charAt(0); 
-//			        switch(option) {
-//			        
-//				        case('1'):
-//				        	saldo.EmitirHistorico(data);
-//				        	break;
-//				        
-//				        
-//				        case('2'):
-//				        	System.out.print("Digite a descricao que sera deletada:\n"); 
-//				        	descricao = read.next();
-//				        	saldo.RemoveHistorico(data, descricao);
-//				        	break;
-//			        }
-//	        		break;
-//	        	case('4'):
-//	        		data = receitaView.ColetarData();
-//	        		System.out.println(saldo.getSaldo(data));
-//	        		break;
-//	        	case('5'):
-//	        		System.out.println(saldo.getTotal());
-//	        		break;
-//	        }
-//		
-//	    //Condicao para sair do Menu
-//		}while(option != '0');
 		//Inicializacao do construtor da classe
 		ArmazenaArquivoControle armazena = new ArmazenaArquivoControle(mapa, filePathMapa, filePathSaldo, saldo);
 		//Armazena dados do Mapa
